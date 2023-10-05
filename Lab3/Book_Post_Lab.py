9aae23e99cf419f42cce5881ca9205fdeeae99d7
Muhammad Anas <anasmuhamad800@gmail.com>
2023-10-05 16:18:06 +0500

    Postlab

---
+++ Lab3/Book_Post_Lab.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.available = True
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            return f'You have borrowed "{self.title}" by {self.author}.'
+        else:
+            return f'"{self.title}" by {self.author} is currently unavailable.'
+
+    def return_book(self):
+        if not self.available:
+            self.available = True
+            return f'You have returned "{self.title}" by {self.author}.'
+        else:
+            return f'"{self.title}" by {self.author} was not borrowed.'
+
+# Example usage:
+book1 = Book('The Great Gatsby', 'F. Scott Fitzgerald')
+print(book1.borrow())
+print(book1.borrow())
+print(book1.return_book())
+print(book1.return_book())
